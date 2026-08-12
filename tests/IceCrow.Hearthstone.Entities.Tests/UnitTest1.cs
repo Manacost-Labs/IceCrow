2ec89e456164703cdd5c90c2a72ba6a0df0b0885
Zulut30 <243011385+Zulut30@users.noreply.github.com>
2026-08-12 23:15:55 +0200

    Initialize .NET solution foundation

---
+++ tests/IceCrow.Hearthstone.Entities.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace IceCrow.Hearthstone.Entities.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/IceCrow.Recording.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace IceCrow.Recording.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
